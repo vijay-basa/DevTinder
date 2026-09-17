c3466ef143c3711d4f502af9e250053b89f7902c
vijay-basa <basavijaykumar52@gmail.com>
2026-09-17 20:18:55 +0530

    auth middleware

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 const express = require("express");
+const { adminAuth, userAuth } = require("./middleware/auth");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-    res.send({ "first_name": "Vijay", "last_name": "Basa" });
+app.use("/admin", adminAuth)
+
+app.get("/user", userAuth, (req, res) => {
+    res.send("User data sent")
 })
 
-app.post("/user", (req, res) => {
-    res.send("Created a User");
+app.post("/user/login", (req, res, next) => {
+    res.send("User logged in successfully")
 })
 
-app.delete("/user", (req, res) => {
-    res.send("Deleted a User");
+app.get("/admin/getAllData", (req, res, next) => {
+    res.send("Send All data")
+})
+
+app.get("/admin/deleteUser", (req, res, next) => {
+    res.send("Deleted a user")
 })
 
 app.listen(7777, () => {
