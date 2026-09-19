e0a498340bcd49eeaddb3a7b283c37714f42905b
vijay-basa <basavijaykumar52@gmail.com>
2026-09-19 17:57:14 +0530

    get, patch and delete APIs

--- src/app.js
+++ src/app.js
@@ -5,10 +5,39 @@ const User = require('./models/user.js');
 const app = express();
 app.use(express.json());
 
+
+// Feed GET API - get all the users from DB
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+})
+
+// User GET API - get user by emailId from DB
+app.get("/user", async (req, res) => {
+  try {
+    const userEmail = req.body.emailId;
+    const users = await User.find({ emailId: userEmail });
+    // const user = await User.findOne({ emailId: userEmail });
+    // const user = await User.findOne();
+    // const user = await User.findById({ _id: "6aae2a5bc19d38a46d02fe6b" });
+    if(users.length === 0) {
+      res.status(404).send("No User Found");
+    }
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+// Signup POST API - signup new user in DB
 app.post('/signup', async (req, res) => {
 
   const user = new User(req.body);
-  
+
   try {
     await user.save();
     res.send("User Added Successfully!");
@@ -17,6 +46,36 @@ app.post('/signup', async (req, res) => {
   }
 
 });
+
+// User PATCH API - update user by Id or emailId in DB
+app.patch("/user", async (req, res) => {
+  try {
+    const data = req.body;
+    const userId = req.body.userId;
+    await User.findByIdAndUpdate(userId, data);
+    // const updateUser = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: 'after' });
+
+    // const userEmail = req.body.emailId;
+    // const updateUser = await User.findOneAndUpdate({ emailId: userEmail }, data, { returnDocument: "before" });
+    // console.log(updateUser);
+
+    res.send("User update success!");
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+})
+
+// User DELETE API - delete user by Id from DB
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    // await User.findByIdAndDelete(userId);
+    await User.findByIdAndDelete({ _id: userId });
+    res.send("User delete success!")
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+})
 
 connectDB()
   .then((data) => {
