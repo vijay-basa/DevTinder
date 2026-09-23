799867a8a127eca24d86dd7c0bc8263930c03366
vijay-basa <basavijaykumar52@gmail.com>
2026-09-23 21:15:29 +0530

    cors middleware

--- src/app.js
+++ src/app.js
@@ -5,10 +5,15 @@ const authRouter = require("./routes/auth.js");
 const profileRouter = require("./routes/profile.js");
 const requestRouter = require("./routes/request.js");
 const userRouter = require("./routes/user.js");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -44,7 +44,7 @@ authRouter.post("/login", async (req, res) => {
     const token = await user.getJWT();
 
     res.cookie("token", token);
-    res.send("Login successfully!");
+    res.send(user);
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
